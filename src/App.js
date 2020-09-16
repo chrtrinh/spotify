dc46691da83f0fa96a22ae6ecf8965f91a5ea520
Christopher Trinh <Chris.trinh@live.com>
2020-09-16 13:19:02 -0400

    initial commit. Authentication with spotify has been setup.

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+	const [token, setToken] = useState(null);
+
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = '';
+		const _token = hash.access_token;
+
+		if (_token) {
+			setToken(_token);
+			spotify.setAccessToken(_token);
+			spotify.getMe().then((user) => {
+				console.log('Show me the user ,', user);
+			});
+		}
+		// console.log('I have a token >>> 👉 ', _token);
+	}, []);
+	return <div className="app">{token ? <Player /> : <Login />}</div>;
+}
+
+export default App;
